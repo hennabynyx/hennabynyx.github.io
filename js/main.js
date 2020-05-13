b7513143e449ae8a247780d451015c08b17c1131
GuruKoushik <guru.koushik@gmail.com>
2020-05-13 01:17:33 -0400

    Added blog posts

--- js/main.js
+++ js/main.js
@@ -273,6 +273,14 @@
   	if (event.target == modal) {
     	modal.style.display = "none";
   	}
+
+  	// Hide navbar when modals trigger
+	  $('.portfolio-modal').on('show.bs.modal', function(e) {
+	    $('.navbar').addClass('d-none');
+	  })
+	  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+	    $('.navbar').removeClass('d-none');
+	  })
 } 
 
 
